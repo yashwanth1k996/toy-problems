01b576951c1ea7598e405c0b51d44e7741e14e5a
Yashwanth-Kumar-Datta-T <ykdatta96@msitprogram.net>
2020-04-14 17:20:52 +0530

    added methods put and get

--- lrucache.py
+++ lrucache.py
@@ -1,3 +1,14 @@
 class LRU:
   def __init__(self, capacity):
-    self.capacity = capacity
+      self.capacity = capacity
+      self.elements = {}
+      self.elementKey = []
+      pass
+  
+
+  def put(self, key, value):
+      pass
+        
+
+  def get(self, value):
+      pass
